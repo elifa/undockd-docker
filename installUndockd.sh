df8bb6d30aeab29816e88888a4ebc6eeff88414c
Elias Faxö <elias.faxo@gmail.com>
2015-09-28 23:45:29 +0200

    Change from Ubuntu to Alpine as base distro.

--- installUndockd.sh
+++ installUndockd.sh
@@ -1,1 +1,3 @@
+#!/usr/bin/env bash
+
 docker create -v /var/lib/postgresql/data --name=undpostgress /bin/true
--- startUndockd.sh
+++ startUndockd.sh
@@ -1,2 +1,4 @@
+#!/usr/bin/env bash
+
 docker run -d --name=undockd-db --volumes-from=undockd-data -e POSTGRES_USER=undockd -e POSTGRES_PASSWORD=undockd
 docker run -d -p 10.10.10.1:14080:80 -v /srv/files/private/elifax/Projects/Undockd:/application/undockd --link undockd-db:postgres --name=undockd elifax/undockd

1340bbda2961f7fd43a6412575bb7d9324d84133
Elias Faxö <elias.faxo@gmail.com>
2015-10-18 16:18:51 +0200

    Clean up of config.

--- startUndockd.sh
+++ startUndockd.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-i
+
 docker run --name Undockd-DB -d mongo --storageEngine wiredTiger
-#docker run -d --name=undockd-db --volumes-from=undockd-data -e POSTGRES_USER=undockd -e POSTGRES_PASSWORD=undockd
-docker run -d -p 10.10.10.1:14080:80 -v /srv/files/private/elifax/Projects/Undockd:/application/undockd --link Undockd-DB:mongo --name Undockd elifax/undockd
+docker run -d -p 10.10.10.1:14080:80 -v /srv/files/private/elifax/Projects/Undockd:/application/undockd -v /var/run/docker.sock:/var/run/docker.sock --link Undockd-DB:mongo --name Undockd elifax/undockd
